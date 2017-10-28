fa165a2bb514baac475cefce9431d70d498cd495
ericnakaw <ericnakaw@hotmail.com>
2017-10-28 18:45:43 -0200

    05.03 - Criando ProjectNote

--- app/Entities/Project.php
+++ app/Entities/Project.php
@@ -10,6 +10,19 @@ class Project extends Model implements Transformable
 {
     use TransformableTrait;
 
-    protected $fillable = [];
+    protected $fillable = [
+        'owner_id',
+        'client_id',
+        'name',
+        'descripton',
+        'progress',
+        'status',
+        'due_date',
+    ];
+    
+    public function notes()
+    {
+        return $this->hasMany(ProjecNote::class);
+    }
 
 }
