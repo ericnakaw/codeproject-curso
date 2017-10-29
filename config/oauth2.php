b2b01716427cbb99d1b2255f303f410c67b85bf6
ericnakaw <ericnakaw@hotmail.com>
2017-10-28 23:40:33 -0200

    06.03 - Criando Rota para o access token

--- config/oauth2.php
+++ config/oauth2.php
@@ -30,7 +30,7 @@ return [
     'grant_types' => [
         'password' => [
             'class' => '\League\OAuth2\Server\Grant\PasswordGrant',
-            'callback' => '\CodeProject\PasswordGrantVerifier@verify',
+            'callback' => '\CodeProject\OAuth\PasswordGrantVerifier@verify',
             'access_token_ttl' => 3600
         ]
     ],
