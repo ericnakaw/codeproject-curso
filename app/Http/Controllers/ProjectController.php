26833d62ce5dea068b6d5ee5a7d09b1a11b2e102
ericnakaw <ericnakaw@hotmail.com>
2017-10-30 23:56:23 -0200

    07.05 - Check Owner e Permissions_x264

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -49,7 +49,7 @@ class ProjectController extends Controller
      */
     public function show($id)
     {
-        if(!$this->checkProjectOwner($id)){
+        if(!$this->checkProjectPermissions($id)){
             return ['error' => 'Acesso Negado'];
         }
         return $this->repository->find($id);
@@ -64,7 +64,7 @@ class ProjectController extends Controller
      */
     public function update(Request $request, $id)
     {
-        if(!$this->checkProjectOwner($id)){
+        if(!$this->checkProjectPermissions($id)){
             return ['error' => 'Acesso Negado'];
         }
         return $this->service->update($request->all(), $id);
@@ -78,7 +78,7 @@ class ProjectController extends Controller
      */
     public function destroy($id)
     {
-        if(!$this->checkProjectOwner($id)){
+        if(!$this->checkProjectPermissions($id)){
             return ['error' => 'Acesso Negado'];
         }
         return $this->repository->delete($id);
@@ -89,5 +89,21 @@ class ProjectController extends Controller
         $userId = \Authorizer::getResourceOwnerId();
         
         return $this->repository->isOwner($projectId,$userId);
+    }
+    
+    private function checkProjectMember($projectId) 
+    {
+        $userId = \Authorizer::getResourceOwnerId();
+        
+        return $this->repository->hasMember($projectId,$userId);
+    }
+    
+    
+    private function checkProjectPermissions($projectId) 
+    {   
+        if( $this->checkProjectOwner($projectId) or $this->checkProjectMember($projectId)){
+            return true;
+        }
+        return false;
     }
 }
