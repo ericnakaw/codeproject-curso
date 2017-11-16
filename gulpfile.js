c479b8a49b66741b072d6533d79b6010fe1dbb89
ericnakaw <ericnakaw@hotmail.com>
2017-11-16 01:14:40 -0200

    11.14 - Tarefa watch-dev

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,8 @@ var elixir = require('laravel-elixir'),
 	gulp = require('gulp');
 
 var config = {
-	assets_path: '.resources/assets',
-	build_path: '.public/build'
+	assets_path: './resources/assets',
+	build_path: './public/build'
 };
 
 config.bower_path = config.assets_path + '/../bower_components';
@@ -21,14 +21,14 @@ config.vendor_path_js = [
 	config.bower_path + '/angular-animate/angular-animate.min.js',
 	config.bower_path + '/angular-messages/angular-messages.min.js',
 	config.bower_path + '/angular-bootstrap/ui-bootstrap.min.js',
-	config.bower_path + '/angular-strap/dist/modules/navbar.min.js',
+	config.bower_path + '/angular-strap/dist/modules/navbar.min.js'
 ];
 
 config.build_path_css = config.build_path + '/css';
 config.build_vendor_path_css = config.build_path_css + '/vendor';
 config.vendor_path_css = [
 	config.bower_path + '/bootstrap/dist/css/bootstrap.min.css',
-	config.bower_path + '/bootstrap/dist/css/bootstrap-theme.min.css',
+	config.bower_path + '/bootstrap/dist/css/bootstrap-theme.min.css'
 ];
 
 gulp.task('copy-styles',function(){
@@ -54,6 +54,13 @@ gulp.task('copy-scripts',function(){
 		.pipe(gulp.dest(config.build_vendor_path_js))
 		.pipe(liveReload());
 });
+
+gulp.task('watch-dev',function(){
+	liveReload.listen();
+	gulp.start('copy-styles','copy-scripts');
+	gulp.watch(config.assets_path + '/**',['copy-styles','copy-scripts']);
+});
+
 /*
  |--------------------------------------------------------------------------
  | Elixir Asset Management
